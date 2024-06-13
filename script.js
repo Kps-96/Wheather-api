c03373966717e41d63f2ca108d8d693808055893
Karuppasamy M <144100827+Kps-96@users.noreply.github.com>
2024-06-13 21:56:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const apiKey = "bab3e1b035f5319c6403fe3e506adb9c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const input = document.getElementById("search-bar");
+const btn = document.getElementById("btn");
+const img = document.getElementById("img")
+
+async function checkWeather(c){
+    const  resp = await fetch(apiUrl + c + `&appid=${apiKey}`);
+    var data = await resp.json();
+
+    console.log(data)
+    const temp =document.getElementById("temp");
+    const city =document.getElementById("city");
+    const humidity =document.getElementById("humidity");
+    const wind =document.getElementById("wind");
+
+    temp.innerHTML=Math.round(data.main.temp)+ "°C";
+    city.innerHTML=data.name;
+    humidity.innerHTML=data.main.humidity+"% <br> Humidity";
+    wind.innerHTML=data.wind.speed+"Km\h <br> Humidity";
+
+    if (data.weather[0].main == "Clouds"){
+        img.src = "/images/clouds.png";
+    }
+    else if(data.weather[0].main =="Clear"){
+        img.src ="/images/clear.png";
+    }
+    else if(data.weather[0].main =="Rain"){
+        img.src ="/images/rain.png";
+    }
+    else if(data.weather[0].main =="Drizzle"){
+        img.src ="/images/drizzle.png";
+    }
+    else if(data.weather[0].main =="Mist"){
+        img.src ="/images/mist.png";
+    }
+
+}
+
+
+btn.addEventListener("click" , () => {
+    if (input.value === '') {
+        alert("Please enter the City name !");
+    }
+
+    checkWeather(input.value);
+    input.value="";
+});
